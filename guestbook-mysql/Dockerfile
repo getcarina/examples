97651d99ef75afebdb400b67d2647d6178338e0f
Everett Toews <everett.toews@rackspace.com>
2015-10-16 15:07:28 -0500

    Guestbook MySQL

---
+++ guestbook-mysql/Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.4
+
+ENV PYTHONUNBUFFERED 1
+
+ADD . /src
+
+WORKDIR /src
+
+RUN pip install -r requirements.txt
+
+EXPOSE 5000
+
+CMD python app.py runserver --host 0.0.0.0
---
+++ guestbook-mysql/app.py
@@ -0,0 +1,64 @@
+import logging
+import os
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+logger.info("Welcome to Guestbook: MySQL Edition")
+
+SQLALCHEMY_DATABASE_URI = \
+    '{engine}://{username}:{password}@{host}:{port}/{database}'.format(
+        engine='mysql+pymysql',
+        username=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        host=os.getenv('MYSQL_HOST'),
+        port=os.getenv('MYSQL_PORT'),
+        database=os.getenv('MYSQL_DATABASE'))
+
+logger.debug("The log statement below is for educational purposes only. Do not log credentials.")
+logger.debug("%s", SQLALCHEMY_DATABASE_URI)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+manager = Manager(app)
+db = SQLAlchemy(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        guest = Guest(name=name)
+        db.session.add(guest)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('index.html', guests=Guest.query.all())
+
+
+class Guest(db.Model):
+    __tablename__ = 'guests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256), nullable=False)
+
+    def __repr__(self):
+        return "[Guest: id={}, name={}]".format(self.id, self.name)
+
+
+@manager.command
+def create_tables():
+    app.config['SQLALCHEMY_ECHO'] = True
+    db.create_all()
+
+
+if __name__ == '__main__':
+    manager.run()
