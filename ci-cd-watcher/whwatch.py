112de73f055addba48d7ab99e7029926daa9539b
Jamie Hannaford <jamie.hannaford@rackspace.com>
2015-10-23 15:46:21 +0200

    cleanup

--- ci-cd-watcher/whwatch.py
+++ ci-cd-watcher/whwatch.py
@@ -1,7 +1,6 @@
 from flask import Flask, session, redirect, url_for, escape, request
-import simplejson as json
-import socket
 import docker
+import sys
 
 app = Flask(__name__)
 app.debug = True
@@ -13,20 +12,17 @@ def hello():
 @app.route('/webhook', methods=['POST'])
 def webhook():
     try:
-        data = request.form
-        repo_name = "donschenck/productws"
         tls_config = docker.tls.TLSConfig(
             client_cert=('/etc/docker/server-cert.pem', '/etc/docker/server-key.pem'), 
             verify='/etc/docker/ca.pem')
-
         cli = docker.Client(base_url='https://swarm-manager:2376', tls=tls_config)
 
         host_config = cli.create_host_config(port_bindings={8080: 8080})
-        container = cli.create_container(image=repo_name, detach=True, host_config=host_config)
+        container = cli.create_container(image="donschenck/productws", detach=True, host_config=host_config)
         response = cli.start(container=container.get('Id'))
 
-    except Exception as e:
-        return "<p>Error: %s</p>" % e.strerror
+    except Exception:
+        return "<p>Error: %s</p>" % sys.exc_info()[0]
     else:
         return "started!"
 
