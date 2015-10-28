98b4ff490e517036fd21631e558c7e48ff5b53e0
Don Schenck <don.schenck@gmail.com>
2015-10-28 11:37:54 -0400

    Refactored; now using Environment Variables.

--- ci-cd-watcher/README.md
+++ ci-cd-watcher/README.md
@@ -1,7 +1,3 @@
 ```
-docker run -d \
-  --volumes-from swarm-data \
-  -p 5000:5000 \
-  --name watcher \
-  <imageName>
+docker run -d -e WHWATCH_IMAGE_NAME="donschenck/productws:latest" --volumes-from swarm-data -p 5000:5000 --name watcher donschenck/whwwatch:latest
 ```
--- ci-cd-watcher/whwatch.py
+++ ci-cd-watcher/whwatch.py
@@ -1,24 +1,25 @@
 from flask import Flask, session, redirect, url_for, escape, request
 import docker
 import sys
+import os
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route("/version", methods=['GET'])
 def hello():
-    return "1.0.0"
+    return "1.0.1"
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     try:
         tls_config = docker.tls.TLSConfig(
-            client_cert=('/etc/docker/server-cert.pem', '/etc/docker/server-key.pem'), 
+            client_cert=('/etc/docker/server-cert.pem', '/etc/docker/server-key.pem'),
             verify='/etc/docker/ca.pem')
         cli = docker.Client(base_url='https://swarm-manager:2376', tls=tls_config)
 
         host_config = cli.create_host_config(port_bindings={8080: 8080})
-        container = cli.create_container(image="donschenck/productws", detach=True, host_config=host_config)
+        container = cli.create_container(image=os.environ["WHWATCH_IMAGE_NAME"], detach=True, host_config=host_config)
         response = cli.start(container=container.get('Id'))
 
     except Exception:
