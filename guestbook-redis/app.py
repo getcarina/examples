ab10f03284c3a78b1b3cbe07f77c53877b24d6a1
Everett Toews <everett.toews@rackspace.com>
2015-11-04 18:10:34 -0600

    Guestbook Redis

---
+++ guestbook-redis/app.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+from flask import Flask, render_template, request, redirect, url_for
+
+from redis import Redis
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+logger.info("Welcome to Guestbook: Redis Edition")
+
+
+host = os.getenv('REDIS_HOST')
+port = int(os.getenv('REDIS_PORT'))
+
+app = Flask(__name__)
+redis = Redis(host=host, port=port)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        redis.lpush('guests', name)
+        return redirect(url_for('index'))
+
+    return render_template('index.html', guests=redis.lrange('guests', 0, -1))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
